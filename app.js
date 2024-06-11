664b919d292e44eec9962c4bfc7753c21a71015e
anjalykrishna522002 <anjalykrishnaas@gmail.com>
2024-06-11 16:17:08 +0530

    done

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const  express = require("express")
-const  mongoose =require("mongoose")
-const  cors =require("cors")
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const { employeemodel } = require("./models/employee")
+
 const app = express()
 app.use(cors())
+app.use(express.json())
 
-app.get("/",(req,res) =>{
-    res.send("hello")
+mongoose.connect("mongodb+srv://anjalykrishna:anjaly@cluster0.vc1zl.mongodb.net/employeeDB?retryWrites=true&w=majority&appName=Cluster0")
+app.post("/add", (req, res) => {
+    let input = req.body
+    let employee = new employeemodel(input)
+    employee.save()
+    console.log(employee)
+    res.json({ "status": "success" })
 })
-app.get("/contact",(req,res)=>{
-    res.send("Welcome to my contact page")
+app.get("/view", (req, res) => {
+    employeemodel.find().then(
+        (data) => {
+            res.json(data)
+        }
+    ).catch(
+        (error) => {
+            res.json(error)
+        }
+    )
 })
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("server started")
 })
--- models/employee.js
+++ models/employee.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose")
 const schema = mongoose.Schema(
     {
-        
-        "employeeId": String,
-        "firsttname":  String,
-       " lastname":  String,
-       " dob": String,
-        
-        "mob":  String,
-        "email":  String,
-        "address":  String
+
+        "employeeId": { type: String, required: true },
+        "firstname": { type: String, required: true },
+        "lastname": { type: String, required: true },
+        "dob": { type: String, required: true },
+        "mob": { type: String, required: true },
+        "email": { type: String, required: true },
+        "address": { type: String, required: true }
     }
 )
 
-let employeemodel=mongoose.model("employees",schema);
-module.exports={employeemodel}
+let employeemodel = mongoose.model("employees", schema);
+module.exports = { employeemodel }
